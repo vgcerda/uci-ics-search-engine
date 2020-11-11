98a34be3de48c1c45df099449c6640bcfec79977
Aznpepsi <Jamesbanh@yahoo.com>
2020-11-10 19:32:06 -0800

    edits

--- indexer.py
+++ indexer.py
@@ -3,14 +3,14 @@ from collections import defaultdict
 from Tokenizer import Tokenize_object
 
 def build_index(documents):
-    indexes = defaultdict(set) #key = token and value = set of URLS
+    indexes = defaultdict(lambda: defaultdict(int)) #key = token and value = set of URLS
     doc_id = 0
     tokenizer = Tokenize_object()
-    for doc in documents:
-        tokenized_doc = tokenizer.tokenize(doc)
+    for json_doc in documents:
+        tokenized_doc = tokenizer.tokenize(json_doc)
         tokens_dict = tokenizer.computeWordFrequencies(tokenized_doc)
         for (token,frequency) in tokens_dict.items():
-            indexes[token].add(tuple("URL from JSON doc", frequency)) #edit the string to get URL from the json doc 
+            indexes[token]["URL from json doc"] = frequency #edit the string to get URL from the json doc 
     return indexes
 
 
