1790b14604ac1a5696da9ad509bc3253c5f696e7
Aznpepsi <Jamesbanh@yahoo.com>
2020-11-10 20:46:39 -0800

    edits

--- indexer.py
+++ indexer.py
@@ -3,10 +3,14 @@ import json
 import re
 from bs4 import BeautifulSoup
 from collections import defaultdict
+import os
+from pathlib import Path
 
-for i in 'abcdefghijklmnopqrstuvwxyz':
-	with open(i + '.json', 'w', encoding='utf-8') as f:
-		json.dump(defaultdict(lambda: defaultdict(int)), f)
+
+def json_buckets():
+	for letter in 'abcdefghijklmnopqrstuvwxyz':
+		with open(letter + '.json', 'w', encoding='utf-8') as f:
+			json.dump(defaultdict(lambda: defaultdict(int)), f)
 
 def tokenize(text, regex):
 	words = defaultdict(int)
@@ -24,6 +28,14 @@ def process_json(json_file):
 		word_json = json.load(word[0] + '.json')
 		word_json[word][url] = frequency
 		json.dump(word[0] + '.json', word_json)
+
+def build_indexer():
+	current_directory = os.getcwd()
+	folder = os.path.join(current_directory, 'ANALYST')
+	for file_name in os.listdir(folder):
+		for json_file in os.listdir(file_name):
+			if json_file.endswith(".json"):
+				process_json(json_file)
 
 
 	# here we iterate through the words and put them in the corresponding files, along with urls
