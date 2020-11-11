de01a142c9e1496058f9c31b154c5691cca07508
vgcerda <vgccerda@gmail.com>
2020-11-10 20:49:53 -0800

    hello

--- indexer.py
+++ indexer.py
@@ -1,5 +1,6 @@
 
 import json
+import os.path
 import re
 from bs4 import BeautifulSoup
 import defaultdict
@@ -19,7 +20,7 @@ def process_json(json_file):
 	url = json_dict["url"]
 	encoding = json_dict["encoding"]
 	soup = BeautifulSoup(json_dict[content], 'html.parser')
-	tokens = tokenize(soup.get_text(), reg=r"[a-zA-Z'-]*[a-zA-Z']+")
+	tokens = tokenize(soup.get_text(), r"[a-zA-Z'-]*[a-zA-Z']+")
 	for word, frequency in tokens:
 		word_json = json.load(word[0] + '.json')
 		word_json[word][url] = frequency
@@ -27,3 +28,4 @@ def process_json(json_file):
 
 
 	# here we iterate through the words and put them in the corresponding files, along with urls
+
