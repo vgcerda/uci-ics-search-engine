7d5aca8bf256296eee87ee0f59ab9b26656d98ec
vgcerda <vgccerda@gmail.com>
2020-11-10 20:12:52 -0800

    basic structure of indexer pt 2

--- indexer.py
+++ indexer.py
@@ -6,7 +6,7 @@ import defaultdict
 
 for i in 'abcdefghijklmnopqrstuvwxyz':
 	with open(i + '.json', 'w', encoding='utf-8') as f:
-		json.dump(dict(), f)
+		json.dump(defaultdict(lambda: defaultdict(int)), f)
 
 def tokenize(text, regex):
 	words = defaultdict(int)
@@ -15,10 +15,15 @@ def tokenize(text, regex):
 	return words
 
 def process_json(json_file):
-	json_dict = json.loads(json_file)
+	json_dict = json.load(json_file)
 	url = json_dict["url"]
 	encoding = json_dict["encoding"]
 	soup = BeautifulSoup(json_dict[content], 'html.parser')
 	tokens = tokenize(soup.get_text(), reg=r"[a-zA-Z'-]*[a-zA-Z']+")
+	for word, frequency in tokens:
+		word_json = json.load(word[0] + '.json')
+		word_json[word][url] = frequency
+		json.dump(word[0] + '.json')
+
 
 	# here we iterate through the words and put them in the corresponding files, along with urls
