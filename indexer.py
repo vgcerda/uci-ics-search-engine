c437e84d504ac43e51555cbbac2a784fc094e480
Aznpepsi <Jamesbanh@yahoo.com>
2020-11-10 20:16:26 -0800

    edits

--- indexer.py
+++ indexer.py
@@ -2,7 +2,7 @@
 import json
 import re
 from bs4 import BeautifulSoup
-import defaultdict
+from collections import defaultdict
 
 for i in 'abcdefghijklmnopqrstuvwxyz':
 	with open(i + '.json', 'w', encoding='utf-8') as f:
@@ -18,7 +18,7 @@ def process_json(json_file):
 	json_dict = json.loads(json_file)
 	url = json_dict["url"]
 	encoding = json_dict["encoding"]
-	soup = BeautifulSoup(json_dict[content], 'html.parser')
-	tokens = tokenize(soup.get_text(), reg=r"[a-zA-Z'-]*[a-zA-Z']+")
+	soup = BeautifulSoup(json_dict["content"], 'html.parser')
+	tokens = tokenize(soup.get_text(), r"[a-zA-Z'-]*[a-zA-Z']+")
 
 	# here we iterate through the words and put them in the corresponding files, along with urls
