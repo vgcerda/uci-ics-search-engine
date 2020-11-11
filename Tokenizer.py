7544e43d1ee21cc1dd30763b995db0e1e8dcc43d
vgcerda <vgccerda@gmail.com>
2020-11-10 19:33:08 -0800

    eddits

--- Tokenizer.py
+++ Tokenizer.py
@@ -3,49 +3,20 @@ import sys
 from collections import defaultdict
 import json
 
-# The tokenize function has a runtime complexity of O(N * (M + P)).
-#    derived from O(1) * O(N) * O(M + P) = O(N * (M + P))
-#    Each component is enumerated in the comments below
-class Tokenize_object:
-    def __init__(self):
-        print("Building Tokenizer")
 
-    def tokenize(self,json_file):
-        try:  
-            f = json.load(json_file) 
-            for line in f:                                      # O(N) Dependent on the number of lines in a file
-                for x in re.finditer(r'[0-9a-zA-Z]+', line):    # O(M) + O(P)
-                                                                # O(M) is the runtime complexity of finditer
-                                                                # O(P) Depends on looping through the number of tokens
-                                                                #    that finditer returns
-                    yield x.group(0).lower()                    # O(1) always happens
-        except IOError:                                             # worst case scenario: never happens
-            print('File not found: {}'.format(json_file))
-            exit()
+def tokenize():
+	json_dict = json.loads(json_file)
+	self.url = json_dict['']
+	for line in f:                                      # O(N) Dependent on the number of lines in a file
+		for x in re.finditer(r'[0-9a-zA-Z]+', line):    # O(M) + O(P)
+			yield x.group(0).lower()                    # O(1) always happens
 
 
 # The computeWordFrequencies function has a runtime complexity of O(N)
 #    derived from O(1) + O(N) * O(1) + O(1) = O(N)
 #    Each component is enumerated in the comments below
-    def computeWordFrequencies(self, tokListGen):
-        frequencies = defaultdict(lambda: 0)    # O(1)
-        for t in tokListGen:                    # O(N) Iteration through all elements of the token list generator
-            frequencies[t] += 1                 # O(1)
-        return frequencies                      # O(1)
-    
-    
-# The printWordFrequencies function has a runtime complexity of O(N log N)
-#    derived from O(N) + O(N log N) = O(N log N)
-#    Each component is enumerated in the comments below
-def printWordFrequencies(freqMap):
-    for t, f in sorted(freqMap.items(), key=lambda x: -x[1]):   # O(N) + O(N log N)
-                                                                # O(N) Iteration through all elements of a list
-                                                                # O(N log N) runtime complexity of sorted function
-        print('{} - {}'.format(t, f))
-    
-
-if __name__ == "__main__":
-    argList = sys.argv
-    if (len(argList) != 2):
-        print("Program needs one input txt file to run")
-        exit()
+def computeWordFrequencies(self, tokListGen):
+	frequencies = defaultdict(lambda: 0)    # O(1)
+	for t in tokListGen:                    # O(N) Iteration through all elements of the token list generator
+		frequencies[t] += 1                 # O(1)
+	return frequencies                      # O(1)
--- indexer.py
+++ indexer.py
@@ -1,17 +1,28 @@
 import json
 from collections import defaultdict
-from Tokenizer import Tokenize_object
+from Tokenizer import Json_Tokenizer
+from bs4 import BeautifulSoup
+
+indices = defaultdict(set)
+
+def process_json(json_file):
+	json_dict = json.loads(json_file)
+	indices
+
+	
+
+
 
 def build_index(documents):
-    indexes = defaultdict(set) #key = token and value = set of URLS
-    doc_id = 0
-    tokenizer = Tokenize_object()
-    for doc in documents:
-        tokenized_doc = tokenizer.tokenize(doc)
-        tokens_dict = tokenizer.computeWordFrequencies(tokenized_doc)
-        for (token,frequency) in tokens_dict.items():
-            indexes[token].add(tuple("URL from JSON doc", frequency)) #edit the string to get URL from the json doc 
-    return indexes
+	indexes = defaultdict(set) #key = token and value = set of URLS
+	doc_id = 0
+	tokenizer = Tokenize_object()
+	for doc in documents:
+		tokenized_doc = tokenizer.tokenize(doc)
+		tokens_dict = tokenizer.computeWordFrequencies(tokenized_doc)
+		for (token,frequency) in tokens_dict.items():
+			indexes[token].add(tuple("URL from JSON doc", frequency)) #edit the string to get URL from the json doc 
+	return indexes
 
 
 
