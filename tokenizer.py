cd40778f83b678cbcae9952f5c83a63075b7196f
vgcerda <vgccerda@gmail.com>
2020-11-16 21:59:02 -0800

    d

--- tokenizer.py
+++ tokenizer.py
@@ -5,10 +5,13 @@ from collections import defaultdict
 ss = SnowballStemmer('english')
 
 def tokenize(text, regex):										# Tokenizer
+	numWords = 0
 	words = defaultdict(int)
 	for word in re.findall(regex, text):
 		if '-' in word or "'" in word:
+			numWords += 1
 			words[word.lower()]+=1
 		else:
+			numWords += 1
 			words[ss.stem(word.lower())]+=1
-	return words
+	return words, numWords
