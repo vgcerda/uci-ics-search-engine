65f8543daf0a3231782144340e1f88db21b1cfde
vgcerda <vgccerda@gmail.com>
2020-12-02 19:05:41 -0800

    d

--- search.py
+++ search.py
@@ -32,35 +32,32 @@ def load_byte_offset_table(byte_offset_table_path):
 		table = json.load(f)
 	return table
 
-class Query:
-	def __init__(self, query_string, index, url_table, byte_offset_table):
+class Search:
+	def __init__(self, query_string, url_table, byte_offset_table):
+		self._query = tokenize_query(query_string, r"[a-zA-Z0-9]+[a-zA-Z0-9'-]*[a-zA-Z0-9]+") #all tokens are stemmed from the query
 		self._url_table = url_table
 		self._byte_offset_table = byte_offset_table
-
-		self._query = tokenize_query(query_string, r"[a-zA-Z0-9]+[a-zA-Z0-9'-]*[a-zA-Z0-9]+") #all tokens are stemmed from the query
-		self._index = index  # Index is an open file on which we can perform seek and readline operations
-
 
 		self.relevant_document_ids = set()
 		self.relevant_postings = {} #result is a dictionary {word:[IDF, {docid:tfidf], word:[IDF, {docid:tfidf], word:[IDF, {docid:tfidf]}
 
-
 		self.query_tfidf = defaultdict(float)
 		self.cosine_similarity = []  # [(docID1, cosine_similarity), (docid2, cosinesimilarity)]
 
-		self._get_relevant_postings()
-		self._calculate_Cosine_Similarity()
-
-	def start(self):
 		current_working_directory = Path(Path.cwd())
 		data_path  = current_working_directory.joinpath('INDEX')
 		index_path = data_path.joinpath("index.txt")
-		url_table_path = current_working_directory.joinpath('URL_LOOKUP_TABLE.json')
-		byte_offset_table_path = current_working_directory.joinpath("BYTE_OFFSET_TABLE.json")
-		# index = load_index(data_path) #Index is a dictionary of all the json files
-		url_table = load_url_lookup_table(url_table_path)
-		byte_offset_table = load_byte_offset_table(byte_offset_table_path)
-		index = open(index_path, 'r', encoding='utf-8')
+
+		self._index = open(index_path, 'r', encoding='utf-8')  # Index is an open file on which we can perform seek and readline operations
+		
+		self._get_relevant_postings()
+		self._calculate_Cosine_Similarity()
+
+		self._index.close()
+
+	def return_results(self):
+		for url, _ in self.cosine_similarity:
+			yield url
 
 	def _get_relevant_postings(self):
 		delete = set()
@@ -78,20 +75,13 @@ class Query:
 		for token in delete:
 			self._query.remove(token)
 
-		# for word in self._query:
-		# 	if word[0].isdigit():
-		# 		bucket = '0'
-		# 	else:
-		# 		bucket = word[0] #bucket is either alpha letter or 0 
-		# 	self.result[word] = self._index[bucket][word] #retrieves data from the word and appends doc IDS and posting values(TFIDF scores )
-
 	def print_results(self, k):
 		for i in range(len(self.cosine_similarity[:k])):
 			docid = self.cosine_similarity[i][0]
 			print(f'{i + 1}. {self._url_table[docid]}')
 
 			# The code below is for testing
-			print(self.cosine_similarity[i][1])
+			# print(self.cosine_similarity[i][1])
 
 
 	def _calculate_Cosine_Similarity(self):
@@ -141,22 +131,18 @@ class Query:
 		self.cosine_similarity = sorted(self.cosine_similarity, key=lambda x: -x[1])
 
 
-# if __name__ == "__main__":
-# 	current_working_directory = Path(Path.cwd())
-# 	data_path  = current_working_directory.joinpath('INDEX')
-# 	index_path = data_path.joinpath("index.txt")
-# 	url_table_path = current_working_directory.joinpath('URL_LOOKUP_TABLE.json')
-# 	byte_offset_table_path = current_working_directory.joinpath("BYTE_OFFSET_TABLE.json")
-# 	# index = load_index(data_path) #Index is a dictionary of all the json files
-# 	url_table = load_url_lookup_table(url_table_path)
-# 	byte_offset_table = load_byte_offset_table(byte_offset_table_path)
-# 	index = open(index_path, 'r', encoding='utf-8')
+if __name__ == "__main__":
+	current_working_directory = Path(Path.cwd())
+	url_table_path = current_working_directory.joinpath('URL_LOOKUP_TABLE.json')
+	byte_offset_table_path = current_working_directory.joinpath("BYTE_OFFSET_TABLE.json")
+	url_table = load_url_lookup_table(url_table_path)
+	byte_offset_table = load_byte_offset_table(byte_offset_table_path)
 
 	query_string = input("Please Enter Your Query: ")
 	start_time = time.time()
-	query = Query(query_string, index, url_table, byte_offset_table) #index is passed into the query class
-	query.print_results(30)
+	query = Search(query_string, url_table, byte_offset_table) #index is passed into the query class
+	query._print_results(30)
 	print("Search Time: {}".format(time.time() - start_time))
 
-	index.close()
+	
 	
--- website.py
+++ website.py
@@ -1,25 +1,40 @@
 from flask import Flask, url_for, render_template, request, redirect
-from search import Query
+from search import Search
+from pathlib import Path
+import json
 
 app = Flask(__name__)
 
+def load_url_lookup_table(url_lookup_table_path):
+	print("LOADING URL TABLE")
+	with open(url_lookup_table_path, 'r', encoding='utf-8') as f:
+		table = json.load(f)
+	return table
+
+def load_byte_offset_table(byte_offset_table_path):
+	print("LOADING BYTE OFFSET TABLE")
+	with open(byte_offset_table_path, 'r', encoding='utf-8') as f:
+		table = json.load(f)
+	return table
+
+current_working_directory = Path(Path.cwd())
+url_table_path = current_working_directory.joinpath('URL_LOOKUP_TABLE.json')
+byte_offset_table_path = current_working_directory.joinpath("BYTE_OFFSET_TABLE.json")
+url_table = load_url_lookup_table(url_table_path)
+byte_offset_table = load_byte_offset_table(byte_offset_table_path)
 
 @app.route('/')
 def base():
-    return render_template('home.html')
-
-
-
-
+	return render_template('home.html')
 
 @app.route('/search',  methods = ["POST", "GET"])
 def s():
-    if request.method == 'POST':
-        search = request.form['query']
-        print(search) #put search buttons here
-    return render_template('search.html')
-
-
+	if request.method == 'POST':
+		query = request.form['query']
+		results = Search(query).return_results()
+	
+	return render_template('search.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+		
+	app.run(debug=True)
